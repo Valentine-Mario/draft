86c042b925e09074c7a78ba82b8f5d7279e2308d
valentine-mario <valentine13400@gmail.com>
2020-07-30 11:53:55 +0100

    added driver

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,36 @@ mod lexer;
 use crate::lexer::*;
 mod parser;
 use crate::parser::*;
+mod driver;
+use crate::driver::*;
+use docopt::Docopt;
+use serde::Deserialize;
+const USAGE: &'static str = "
+Usage: iron_kaleidoscope [(-l | -p | -i)]
+
+Options:
+    -l  Run only lexer and show its output.
+    -p  Run only parser and show its output.
+    -i  Run only IR builder and show its output.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_l: bool,
+    flag_p: bool,
+    flag_i: bool
+}
+
 fn main() {
-    println!("Hello, world!");
-   println!("{:?}", tokenize(&"def(),;[]pole(hi)34externdef"));
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
+    let stage = if args.flag_l {
+        Tokens
+    } else {
+        AST
+    };
+
+    main_loop(stage);
 }
